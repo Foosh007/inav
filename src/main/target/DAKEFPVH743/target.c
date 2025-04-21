2ccfa8a9345c4cf6a99e9db91d9aa79bae90019f
Foosh007 <foosh007@gmail.com>
2025-04-21 23:52:51 +1000

    adjuste motor timers

--- src/main/target/DAKEFPVH743/target.c
+++ src/main/target/DAKEFPVH743/target.c
@@ -36,16 +36,16 @@
  timerHardware_t timerHardware[] = {
     
     // Motors 1-4
-    DEF_TIM(TIM2, CH1, PA0, TIM_USE_MOTOR, 0, 0), // M1
-    DEF_TIM(TIM2, CH2, PA1, TIM_USE_MOTOR, 0, 1), // M2
-    DEF_TIM(TIM2, CH3, PA2, TIM_USE_MOTOR, 0, 2), // M3
-    DEF_TIM(TIM2, CH4, PA3, TIM_USE_MOTOR, 0, 3), // M4
+    DEF_TIM(TIM5, CH1, PA0, TIM_USE_OUTPUT_AUTO, 0, 0), // M1
+    DEF_TIM(TIM5, CH2, PA1, TIM_USE_OUTPUT_AUTO, 0, 0), // M2
+    DEF_TIM(TIM5, CH3, PA2, TIM_USE_OUTPUT_AUTO, 0, 0), // M3
+    DEF_TIM(TIM5, CH4, PA3, TIM_USE_OUTPUT_AUTO, 0, 0), // M4
 
     // Motors 5-7
-    DEF_TIM(TIM3, CH1, PC6, TIM_USE_MOTOR, 0, 4), // M5
-    DEF_TIM(TIM3, CH2, PC7, TIM_USE_MOTOR, 0, 5), // M6
-    DEF_TIM(TIM8, CH3, PC8, TIM_USE_MOTOR, 0, 6), // M7
-    DEF_TIM(TIM8, CH4, PC9, TIM_USE_MOTOR, 0, 7), // M8
+    DEF_TIM(TIM3, CH1, PC6, TIM_USE_OUTPUT_AUTO, 0, 0), // M5
+    DEF_TIM(TIM3, CH2, PC7, TIM_USE_OUTPUT_AUTO, 0, 0), // M6
+    DEF_TIM(TIM8, CH3, PC8, TIM_USE_OUTPUT_AUTO, 0, 0), // M7
+    DEF_TIM(TIM8, CH4, PC9, TIM_USE_OUTPUT_AUTO, 0, 0), // M8
 
     // Servo outputs - note there are S3/S4 pads on board but dont appear to go to any pins.
     DEF_TIM(TIM15, CH1, PE5, TIM_USE_OUTPUT_AUTO, 0, 0), // S1
@@ -54,8 +54,6 @@
     // LED strip
     DEF_TIM(TIM1, CH1, PE9, TIM_USE_LED, 0, 9), // LED
 
-    // CAM
-    DEF_TIM(TIM16, CH1, PB8, TIM_USE_ANY, 0, 0), // CAM
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
